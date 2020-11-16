652361ba4bfec58f1be5dddbfcb71a1f080bf0d8
Ivan <ivangomezvelazquez96@gmail.com>
2020-11-16 16:24:55 -0300

    Initial Commit

---
+++ ClimApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClimApp
+//
+//  Created by Ivan on 16/11/2020.
+//  Copyright © 2020 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
